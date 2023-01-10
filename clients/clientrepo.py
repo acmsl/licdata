4b55f393e873bb9bdc926fc2ad8be087f2d19de0
Jose San Leandro <jose@io.builders>
2023-01-10 07:11:31 +0100

    Refactored githubrepo and clientrepo

--- clients/clientrepo.py
+++ clients/clientrepo.py
@@ -4,162 +4,35 @@ import githubrepo
 
 
 def findById(clientId):
-    repo = githubrepo.getRepo()
-    branch = githubrepo.getBranch()
-    client = None
-    try:
-        file = repo.get_contents(f"clients/{clientId}/data.json", ref=branch)
-    except:
-        file = None
-    if file:
-        client = json.loads(file.decoded_content.decode())
-    return client
+    return githubrepo.findById(clientId, "clients")
 
 
 def findByEmail(email):
-    repo = githubrepo.getRepo()
-    branch = githubrepo.getBranch()
-    try:
-        allClients = repo.get_contents("clients/data.json", ref=branch)
-    except:
-        allClients = None
-    if allClients:
-        allClientsContent = json.loads(allClients.decoded_content.decode())
-        clients = [x for x in allClientsContent if x["email"] == email]
-        if clients:
-            return clients[0]
-        else:
-            print(f"No client found for email {email}")
-
-    return None
+    return githubrepo.findByAttribute(email, "email", "clients")
 
 
 def insert(email, address, contact, phone):
-    repo = githubrepo.getRepo()
-    branch = githubrepo.getBranch()
-    result = githubrepo.newId()
-
     item = {}
-    item["id"] = result
     item["email"] = email
-    try:
-        file = repo.get_contents("clients/data.json", ref=branch)
-    except:
-        file = None
-    if file is None:
-        content = []
-        content.append(item)
-        repo.create_file(
-            "clients/data.json", "First client", json.dumps(content), branch=branch
-        )
-        item["address"] = address
-        item["contact"] = contact
-        item["phone"] = phone
-        repo.create_file(
-            f"clients/{result}/data.json",
-            f"Created {result} client",
-            json.dumps(item),
-            branch=branch,
-        )
-    else:
-        content = json.loads(file.decoded_content.decode())
-        el = [x for x in content if x["email"] == email]
-        if el:
-            result = el[0]["id"]
-        else:
-            content.append(item)
-            repo.update_file(
-                "clients/data.json",
-                "acmsl-licdata",
-                json.dumps(content),
-                file.sha,
-                branch=branch,
-            )
-            item["address"] = address
-            item["contact"] = contact
-            item["phone"] = phone
-            repo.create_file(
-                f"clients/{result}/data.json",
-                f"Created {result} client",
-                json.dumps(item),
-                branch=branch,
-            )
-    return result
+    item["address"] = address
+    item["contact"] = contact
+    item["phone"] = phone
+    return githubrepo.insert(
+        item, "clients", ["email"], ["email", "address", "contact", "phone"]
+    )
 
 
 def update(id, email, address, contact, phone):
-    repo = githubrepo.getRepo()
-    branch = githubrepo.getBranch()
-
     item = {}
     item["id"] = id
     item["email"] = email
-
-    try:
-        data = repo.get_contents("clients/data.json", ref=branch)
-    except:
-        data = None
-    if data:
-        content = json.loads(data.decoded_content.decode())
-        existing = [x for x in content if x["id"] == id]
-        if existing and existing[0]["email"] != email:
-            remaining = [x for x in content if x["id"] != id]
-            remaining.append(item)
-            repo.update_file(
-                "clients/data.json",
-                f"Updated clients/data.json for client {id}",
-                json.dumps(remaining),
-                data.sha,
-                branch=branch,
-            )
-    print(f"About to update clients/{id}/data.json")
-    try:
-        client = repo.get_contents(f"clients/{id}/data.json", ref=branch)
-        print("client file found")
-    except:
-        client = None
-    if client:
-        item["email"] = email
-        item["address"] = address
-        item["contact"] = contact
-        item["phone"] = phone
-        print(json.dumps(item))
-        repo.update_file(
-            f"clients/{id}/data.json",
-            f"Updated {id} client",
-            json.dumps(item),
-            client.sha,
-            branch=branch,
-        )
-    else:
-        print(f"clients/{id}/data.json not found")
-
-    return item
+    item["address"] = address
+    item["contact"] = contact
+    item["phone"] = phone
+    return githubrepo.update(
+        item, "clients", ["email"], ["email", "address", "contact", "phone"]
+    )
 
 
 def delete(id):
-    repo = githubrepo.getRepo()
-    branch = githubrepo.getBranch()
-
-    try:
-        data = repo.get_contents("clients/data.json", ref=branch)
-    except:
-        data = None
-    if data:
-        content = json.loads(data.decoded_content.decode())
-        el = [x for x in content if x["id"] != id]
-        repo.update_file(
-            "clients/data.json",
-            "Deleted client {id} from clients/data.json",
-            json.dumps(el),
-            data.sha,
-            branch=branch,
-        )
-    try:
-        client = repo.get_contents(f"clients/{id}/data.json", ref=branch)
-    except:
-        client = None
-    if client:
-        repo.delete_file(
-            f"clients/{id}/data.json", f"Deleted {id} client", client.sha, branch=branch
-        )
+    return githubrepo.delete(id, "clients")
--- common/githubrepo.py
+++ common/githubrepo.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 import os
 from github import Github
+import json
 
 
 def getRepo():
@@ -16,3 +17,192 @@ def getBranch():
 
 def newId():
     return str(uuid4())
+
+
+def findById(id, entityType):
+    repo = getRepo()
+    branch = getBranch()
+    item = None
+    try:
+        file = repo.get_contents(f"{entityType}/{id}/data.json", ref=branch)
+    except:
+        file = None
+    if file:
+        item = json.loads(file.decoded_content.decode())
+    return item
+
+
+def findByAttribute(attributeValue, attributeName, entityType):
+    repo = githubrepo.getRepo()
+    branch = githubrepo.getBranch()
+    result = None
+
+    try:
+        allItems = repo.get_contents(f"{entityType}/data.json", ref=branch)
+    except:
+        allItems = None
+    if allClients:
+        allItemsContent = json.loads(allItems.decoded_content.decode())
+        items = [x for x in allItemsContent if x.get(attributeName) == attributeValue]
+        if items:
+            result = items[0]
+        else:
+            print(f"No {entityType} with {attributeName} {attributeValue}")
+
+    return result
+
+
+def insert(entity, entityType, filterKeys, attributeNames):
+    repo = githubrepo.getRepo()
+    branch = githubrepo.getBranch()
+    result = None
+    item = {}
+    for attribute in filterKeys:
+        item[attribute] = entity.get(attribute)
+
+    try:
+        file = repo.get_contents(f"{entityType}/data.json", ref=branch)
+    except:
+        file = None
+    if file is None:
+        result = githubrepo.newId()
+        item["id"] = result
+        content = []
+        content.append(item)
+        repo.create_file(
+            f"{entityType}/data.json",
+            f"First instance in {entityType} collection",
+            json.dumps(content),
+            branch=branch,
+        )
+        nonFilterKeyAttributes = [
+            attr for attr in attributeNames if attr not in filterKeys
+        ]
+        for attribute in nonFilterKeyAttributes:
+            item[attribute] = entity.get(attribute)
+        repo.create_file(
+            f"{entityType}/{result}/data.json",
+            f"Created a new {entityType} entry with id {result}",
+            json.dumps(item),
+            branch=branch,
+        )
+    else:
+        content = json.loads(file.decoded_content.decode())
+        entries = [x for x in content if _attributesMatch(x, entity, filterKeys)]
+        if entries:
+            result = entries[0]["id"]
+        else:
+            result = githubrepo.newId()
+            item["id"] = result
+            content.append(item)
+            repo.update_file(
+                f"{entityType}/data.json",
+                f"Updated {result} in {entityType} collection",
+                json.dumps(content),
+                file.sha,
+                branch=branch,
+            )
+            for attribute in attributeNames:
+                item[attribute] = entity.get(attribute)
+            repo.create_file(
+                f"{entityType}/{result}/data.json",
+                f"Created a new entry {result} in {entityType} collection",
+                json.dumps(item),
+                branch=branch,
+            )
+    return result
+
+
+def _attributesMatch(item, target, attributeNames):
+    result = True
+
+    for attributeName in attributeNames:
+        if item.get(attributeName) != target.get(attributeName):
+            result = False
+            break
+
+    return result
+
+
+def update(entity, entityType, filterKeys, attributeNames):
+    repo = githubrepo.getRepo()
+    branch = githubrepo.getBranch()
+
+    id = entity.get("id")
+    item = {}
+    for attribute in filterKeys:
+        item[attribute] = entity.get(attribute)
+
+    try:
+        data = repo.get_contents(f"{entityType}/data.json", ref=branch)
+    except:
+        data = None
+    if data:
+        content = json.loads(data.decoded_content.decode())
+        existing = [x for x in content if x.get("id") == id]
+        if existing and not _attributesMatch(existing[0], entity, attributeNames):
+            remaining = [x for x in content if x.get("id") != id]
+            remaining.append(item)
+            repo.update_file(
+                f"{entityType}/data.json",
+                f"Updated {id} in {entityType}/data.json",
+                json.dumps(remaining),
+                data.sha,
+                branch=branch,
+            )
+    try:
+        oldItem = repo.get_contents(f"{entityType}/{id}/data.json", ref=branch)
+    except:
+        oldItem = None
+    if oldItem:
+        nonFilterKeyAttributes = [
+            attr for attr in attributeNames if attr not in filterKeys
+        ]
+        for attribute in nonFilterKeyAttributes:
+            item[attribute] = entity.get(attribute)
+        repo.update_file(
+            f"{entityType}/{id}/data.json",
+            f"Updated {entityType}/{id}/data.json",
+            json.dumps(item),
+            oldItem.sha,
+            branch=branch,
+        )
+    else:
+        print(f"{entityType}/{id}/data.json not found")
+
+    return item
+
+
+def delete(id, entityType):
+    repo = githubrepo.getRepo()
+    branch = githubrepo.getBranch()
+    result = False
+
+    try:
+        data = repo.get_contents(f"{entityType}/data.json", ref=branch)
+    except:
+        data = None
+    if data:
+        content = json.loads(data.decoded_content.decode())
+        existing = [x for x in content if x.get("id") != id]
+        repo.update_file(
+            f"{entityType}/data.json",
+            "Deleted {id} from {entityType}/data.json",
+            json.dumps(existing),
+            data.sha,
+            branch=branch,
+        )
+    try:
+        item = repo.get_contents(f"{entityType}/{id}/data.json", ref=branch)
+    except:
+        item = None
+    if item:
+        repo.delete_file(
+            f"{entityType}/{id}/data.json",
+            f"Deleted {entityType}/{id}",
+            client.sha,
+            branch=branch,
+        )
+        result = True
+
+    return result
